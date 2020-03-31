1b90d5dee2824ee7b1018382c0676d981961f0e8
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-03-31 13:30:30 +0530

    triangle/not

---
+++ Conditionals/triangle.py
@@ -0,0 +1,7 @@
+a1=int(input("Enter the angle 1:"))
+a2=int(input("Enter the angle 2:"))
+a3=int(input("Enter the angle 3:"))
+if (a1+a2+a3)==180:
+    print("Triangle")
+else:
+    print("Not a Triangle")
