b7c051ae84a78fa5cdee9e666510a02cae8eed5d
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-03-31 14:20:38 +0530

    roots of quadratic eqn

---
+++ Conditionals/rootsOfQuadraticEqn.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the value of a:"))
+b=int(input("Enter the value of b:"))
+c=int(input("Enter the value of c:"))
+root1=((-b)+(((b**2)-(4*a*c))**0.5))/(2*a)
+root2=((-b)-(((b**2)-(4*a*c))**0.5))/(2*a)
+print(root1)
+print(root2)
