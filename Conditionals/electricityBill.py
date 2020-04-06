568e8d5a38abc14e947c910beb60418729b64c66
priyadharshini-27 <61590245+priyadharshini-27@users.noreply.github.com>
2020-04-07 00:00:59 +0530

    Electricity Bill

---
+++ Conditionals/electricityBill.py
@@ -0,0 +1,11 @@
+electricity_unit=int(input("Enter the units consumed :"))
+if electricity_unit<=50:
+    charge=electricity_unit*0.50
+elif electricity_unit>50 and electricity_unit<=150:
+    charge=25+(electricity_unit-50)*0.75
+elif electricity_unit>150 and electricity_unit<=250:
+    charge=75+(electricity_unit-150)*1.20
+elif electricity_unit>250:
+    charge=200+(electricity_unit-250)*1.5
+subcharge=(0.2)*charge
+print(charge+subcharge)
